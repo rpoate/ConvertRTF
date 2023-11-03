6c66086a344abc664a04c99efa590bf7704c9d98
Richard Poate <info@zoople.com.au>
2023-11-04 10:25:03 +1100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using RtfPipe;
+using System.Windows.Forms;
+
+namespace ConvertRTF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var btn = this.htmlEditControl1.ToolStripItems.Add("Import RTF");
+            btn.Padding = new Padding(3);
+            btn.Click += Btn_Click;
+
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "rtf files (*.rtf)|*.rtf|All files (*.*)|*.*";
+            DialogResult oResult = openDialog.ShowDialog();
+
+            if (oResult == DialogResult.OK)
+            {
+                Cursor currentCursor = Cursor.Current;
+                Cursor.Current = Cursors.WaitCursor;
+
+                string RTFString = System.IO.File.ReadAllText(openDialog.FileName);
+                var HTML = Rtf.ToHtml(RTFString);
+
+                this.htmlEditControl1.DocumentHTML = HTML;
+                Cursor.Current = currentCursor;
+            }
+        }
+    }
+}
